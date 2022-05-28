986c4a2d2e58b8a31dd89b185b2e5ab9566c3df8
mohtareq <mtareq471@gmail.com>
2022-05-28 15:59:18 +0300

    Set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi Mohammed</h1>
+      <p>Hi</p>
+      <p>Hi</p>
     </div>
   );
 }
