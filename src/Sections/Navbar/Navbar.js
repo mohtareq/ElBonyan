1f2bc6e9d07fa58182d131231ec2363f92ed12f3
mohtareq <mtareq471@gmail.com>
2022-06-04 10:59:57 +0300

    Burger Done

--- src/Sections/Navbar/Navbar.js
+++ src/Sections/Navbar/Navbar.js
@@ -2,11 +2,13 @@ import Topics from '../../Components/NavTopics/NavTopics';
 import Logo from '../../Components/NavLogo/NavLogo';
 
 import { NavBar } from './NavbarStyle';
+import BurgerMenu from '../../Components/Burger/Burger';
 
 const Nav = () => {
 
     return(
         <NavBar>
+            <BurgerMenu />
             <Topics />
             <Logo />
         </NavBar>
